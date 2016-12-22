86a9d51aaaa3ff75134a3e00c023d0ce5074df43
georgy <9bd-g8F>
2016-12-22 15:59:50 +0300

    task 3, stage 1

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,29 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import {Routes, RouterModule} from '@angular/router';
 import { FormsModule }   from '@angular/forms';
 
-//import { AppComponent }   from './app.component';
+import { AppComponent }   from './app.component';
 import { LoginComponent }  from './login.component';
-/*
 import { CoursesComponent }  from './сourses.component';
 import { AddEditCourseComponent }  from './add-edit-courses.component';
 
 const appRoutes: Routes =[
-    { path: '', redirectTo: '/login', component: LoginComponent},
-    { path: '/courses', component: CoursesComponent},
-    { path: '/courses/1', component: AddEditCourseComponent }
+	{ path: 'login', component: LoginComponent},
+    { path: '', redirectTo: '/login', pathMatch:'full' },
+    { path: 'courses', component: CoursesComponent, pathMatch:'full' },
+	{ path: 'courses/:id', component: AddEditCourseComponent}
+    
 ];
+ 
 
 @NgModule({
   imports:      [ BrowserModule, RouterModule.forRoot(appRoutes) ],
   declarations: [ AppComponent, LoginComponent, CoursesComponent, AddEditCourseComponent ],
-  bootstrap:    [ AppComponent, LoginComponent ]
+  bootstrap:    [ AppComponent ]
 })
-*/
 
+
+/*
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ LoginComponent ],
   bootstrap:    [ LoginComponent ]
 })
+*/
 
 export class AppModule { }
