cdacfcd576e7bf7b1ff90058c3336d87d0becdf4
georgy <9bd-g8F>
2017-01-09 14:20:55 +0300

    Improve date.component.ts

--- src/app/date.component.ts
+++ src/app/date.component.ts
@@ -2,35 +2,60 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'my-date',
-	template: `<input type='text' [(ngModel)]="DataString" (ngModelChange)="OnChange()">`
+	template: `<input type='text' [(ngModel)]="dataString" (keydown)="keyDown($event)">`
  })
 
  export class DateComponent  { 
 
-	public DataString: string = '';
+	public dataString: string = '';
+    private specialKeys: string[] = ['ArrowLeft', 'ArrowRight', 'Delete', 'Backspace', 'Home', 'End'];
     private numbers: string[] = '0123456789'.split('');
 
-    OnChange()
+    keyDown(e)
     {
-        this.DataString = 'Test';
-        /*
-        var temp: string = '';
-        for(var i:number = 0;  i < 10 && i < this.DataString.length; i++)
+        console.log(e.target.selectionStart);
+        console.log(this.dataString);
+        console.log(e);
+        let ctrlV = e.ctrlKey && e.code == 'KeyV';
+        let isSpecial = ctrlV || this.specialKeys.indexOf(e.code) > -1;
+        if (isSpecial)
         {
-            if (i == 2 || i == 5)
+            setTimeout(() => this.reCheck(e.code == 'Backspace'), 0);
+            return;
+        }
+        if (this.numbers.indexOf(e.key) == -1 || this.dataString.length > 9)
+        {
+            e.preventDefault();
+        }
+        else
+        {
+            setTimeout(() => this.reCheck(), 0);
+        }
+    }
+
+    reCheck(isBackspace: boolean = false)
+    {
+        var temp: string = '';
+        for(var i:number = 0;  i < 10 && i < this.dataString.length; i++)
+        {
+            if (this.numbers.indexOf(this.dataString[i]) > -1)
+            {
+                temp += this.dataString[i];
+            }
+
+            if (isBackspace)
+            {
+                if ((i + 1 < this.dataString.length) && (i == 1 || i == 4))
+                {
+                    temp += '.';
+                }
+            }
+            else if (i == 1 || i == 4)
             {
                 temp += '.';
             }
-            else
-            {
-                if (this.numbers.indexOf(this.DataString[i]) > -1)
-                {
-                    temp += this.DataString[i];
-                }
-            }
         }
-        this.DataString = temp;
-        */
+        this.dataString = temp;
     }
 	
 }
